00717f41d73eae08084b037a0bc004f874651b27
Knipp Marc <Marc.Knipp@swisscom.com>
2022-08-25 19:19:34 +0200

    initial commit

---
+++ src/test/java/com/example/marck/OrderingService/OrderingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marck.OrderingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
